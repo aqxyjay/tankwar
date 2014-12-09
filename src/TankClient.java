a034a3651f848adc584ccc6a611ffe0374b9739b
Aq <aqxyjay@gmail.com>
2014-12-09 22:53:26 +0800

    commit project

---
+++ src/TankClient.java
@@ -0,0 +1,18 @@
+import java.awt.Frame;
+
+public class TankClient extends Frame {
+	
+	private static final long serialVersionUID = 6254741544943336854L;
+
+	public void lauchFrame() {
+		this.setLocation(400, 300);
+		this.setSize(800, 600);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		TankClient tc = new TankClient();
+		tc.lauchFrame();
+	}
+
+}
