c0ea74fe8e3fb3cf177e22d6928d82addaca648b
Aq <aqxyjay@gmail.com>
2014-12-15 22:38:22 +0800

    update
    1. update tankwar
    2. update gitig
    3. update iml

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,18 +1,62 @@
-import java.awt.Frame;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
-	
-	private static final long serialVersionUID = 6254741544943336854L;
 
-	public void launchFrame() {
-		this.setLocation(400, 300);
-		this.setSize(800, 600);
-		setVisible(true);
-	}
+    private static final long serialVersionUID = 6254741544943336854L;
 
-	public static void main(String[] args) {
-		TankClient tc = new TankClient();
-		tc.launchFrame();
-	}
+    int x = 50, y = 50;
 
+    // Ë«»º´æ±³¾°Í¼Æ¬
+    Image offScreenImage = null;
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+
+        y += 5;
+    }
+
+    @Override
+    public void update(Graphics g) {
+        super.update(g);
+    }
+
+    public void launchFrame() {
+        this.setLocation(400, 300);
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setBackground(Color.LIGHT_GRAY);
+        setVisible(true);
+
+        new Thread(new PaintThread()).start();
+    }
+
+    public static void main(String[] args) {
+        TankClient tc = new TankClient();
+        tc.launchFrame();
+    }
+
+    private class PaintThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    repaint();
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
