7365bb974c45bcf3be3524e57d165418f08a6d81
Aq <aqxyjay@gmail.com>
2014-12-20 00:14:09 +0800

    ver 0.7

---
+++ src/Tank.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by aqxyj_000 on 2014/12/19.
+ */
+public class Tank {
+
+    int x;
+    int y;
+    Color color;
+
+    public Tank(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public void draw(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(color);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_LEFT:
+                x -= 5;
+                break;
+            case KeyEvent.VK_UP:
+                y -= 5;
+                break;
+            case KeyEvent.VK_RIGHT:
+                x += 5;
+                break;
+            case KeyEvent.VK_DOWN:
+                y += 5;
+                break;
+        }
+    }
+}
--- src/TankClient.java
+++ src/TankClient.java
@@ -6,20 +6,19 @@ import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
 
-    private static final long serialVersionUID = 6254741544943336854L;
-
     public static final int GAME_WIDTH = 800;
     public static final int GAME_HEIGHT = 600;
-
-    int x = 50, y = 50;
-
+    private static final long serialVersionUID = 6254741544943336854L;
     Image offScreenImage = null;
+    Tank myTank = null;
+
+    public static void main(String[] args) {
+        TankClient tc = new TankClient();
+        tc.launchFrame();
+    }
 
     public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.RED);
-        g.fillOval(x, y, 30, 30);
-        g.setColor(c);
+        myTank.draw(g);
     }
 
     @Override
@@ -47,15 +46,15 @@ public class TankClient extends Frame {
             }
         });
         this.setBackground(Color.LIGHT_GRAY);
-        this.addKeyListener(new KeyMonitor());
+        this.addMyTank();
         setVisible(true);
 
         new Thread(new PaintThread()).start();
     }
 
-    public static void main(String[] args) {
-        TankClient tc = new TankClient();
-        tc.launchFrame();
+    private void addMyTank() {
+        myTank = new Tank(50, 50, Color.RED);
+        this.addKeyListener(new KeyMonitor());
     }
 
     private class PaintThread implements Runnable {
@@ -75,21 +74,7 @@ public class TankClient extends Frame {
     private class KeyMonitor extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            int key = e.getKeyCode();
-            switch (key) {
-                case KeyEvent.VK_LEFT:
-                    x -= 5;
-                    break;
-                case KeyEvent.VK_UP:
-                    y -= 5;
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    x += 5;
-                    break;
-                case KeyEvent.VK_DOWN:
-                    y += 5;
-                    break;
-            }
+            myTank.keyPressed(e);
         }
     }
 }
